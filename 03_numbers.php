9fbe44bf06e06868170d85e97c6623bffea94152
Gulam Anas <g.anas786@gmail.com>
2022-01-27 22:34:03 +0530

    working with numbers

--- 03_numbers.php
+++ 03_numbers.php
@@ -1,10 +1,26 @@
 <?php
 
 // Declaring numbers
+$a = 5;
+$b = 4;
+$c = 1.2;
 
 // Arithmetic operations
+echo ($a + $b) * $c . '<br>';
+echo $a - $b . '<br>';
+echo $a * $b . '<br>';
+echo $a / $b . '<br>';
+echo $a % $b . '<br>';
 
 // Assignment with math operators
+// $a = $a + $b; can be written as :
+$a += $b;
+echo $a . '<br>'; // $a = 9
+// $a -= $b; echo $a.'<br>'; // $a = 9
+// $a *= $b; echo $a.'<br>'; // $a = 9
+// $a /= $b; echo $a.'<br>'; // $a = 9
+// $a %= $b; echo $a.'<br>'; // $a = 9
+
 
 // Increment operator
 
