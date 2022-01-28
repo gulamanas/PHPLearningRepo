68efc06623e1787b748e592d8d2b55d1737c0305
Gulam Anas <g.anas786@gmail.com>
2022-01-28 21:25:17 +0530

    learned numbers in php

--- 03_numbers.php
+++ 03_numbers.php
@@ -14,24 +14,48 @@ echo $a % $b . '<br>';
 
 // Assignment with math operators
 // $a = $a + $b; can be written as :
-$a += $b;
-echo $a . '<br>'; // $a = 9
+// $a += $b;
+// echo $a . '<br>'; // $a = 9
 // $a -= $b; echo $a.'<br>'; // $a = 9
 // $a *= $b; echo $a.'<br>'; // $a = 9
 // $a /= $b; echo $a.'<br>'; // $a = 9
 // $a %= $b; echo $a.'<br>'; // $a = 9
 
-
 // Increment operator
+echo $a++ . '<br>';
+echo ++$a . '<br>';
 
 // Decrement operator
+echo $a-- . '<br>';
+echo --$a . '<br>';
 
 // Number checking functions
+is_float(1.25); // true
+is_double(1.25); // true
+is_int(5); // true
+is_numeric("3.45"); // true
+is_numeric("3g.45"); // false
 
 // Conversion
+$strNumber = '12.34';
+$number = (float)$strNumber;
+// $number = intval($strNumber);
+var_dump($number);
+echo '<br>';
 
 // Number functions
+echo "abs(-15)" . abs(-15) . '<br>';
+echo "pow(2,3)" . pow(2, 3) . '<br>';
+echo "sqrt(16)" . sqrt(16) . '<br>';
+echo "max(2,3)" . max(2, 3) . '<br>';
+echo "min(2,3)" . min(2, 3) . '<br>';
+echo "round(2.4)" . round(2.4) . '<br>';
+echo "round(2.6)" . round(2.6) . '<br>';
+echo "floor(2.6)" . floor(2.6) . '<br>';
+echo "ceil(2.4)" . ceil(2.4) . '<br>';
 
 // Formatting numbers
+$number = 123456789.12345; // 
+echo number_format($number, 2, '.', ',');
 
 // https://www.php.net/manual/en/ref.math.php
