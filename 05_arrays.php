640f76ad061db51989ab4bdcdf3d11ebac13ebc4
Gulam Anas <g.anas786@gmail.com>
2022-01-29 23:01:06 +0530

    associative arrays

--- 05_arrays.php
+++ 05_arrays.php
@@ -98,23 +98,71 @@ echo '</pre>';
 
 // ============================================
 // Associative arrays
+// key value pairs
 // ============================================
 
 // Create an associative array
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Video Game'],
+];
+echo '<pre>';
+// print_r($person);
+var_dump($person);
+echo '</pre>';
 
 // Get element by key
+echo $person['name'] . '<br>';
 
 // Set element by key
+$person['Channel'] = 'TraversyMedia';
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 
 // Null coalescing assignment operator. Since PHP 7.4
+// if (!isset($person['address'])) {
+//     $person['address'] = 'unknown';
+// }
+// $person['address'] = $person['address'] ?? 'unknown';
+$person['address']  ??= 'unknown';
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 
 // Check if array has specific key
 
+
 // Print the keys of the array
+echo '<pre>';
+var_dump(array_keys($person));
+echo '</pre>';
 
 // Print the values of the array
+echo '<pre>';
+var_dump(array_values($person));
+echo '</pre>';
 
 // Sorting associative arrays by values, by keys
+// sorting by keys using ksort
+ksort($person);
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
+// sorting by values using asort
+asort($person);
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 
 
 // Two dimensional arrays
+$todos = [
+    ['title' => 'Todo title 1', 'completed' => true],
+    ['title' => 'Todo title 2', 'completed' => false],
+];
+echo '<pre>';
+var_dump($todos);
+echo '</pre>';
