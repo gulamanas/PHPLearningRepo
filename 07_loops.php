ac591ab9f0965a742629312e099d47ea90795af1
Gulam Anas <g.anas786@gmail.com>
2022-01-30 12:53:17 +0530

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,48 @@
 <?php
 
 // while
+// while (true) {
+
+// }
 
 // Loop with $counter
+$counter = 0;
+// while ($counter < 10) {
+//     echo $counter . '<br>';
+//     // if ($counter === 5) {
+//     //     break;
+//     // }
+//     $counter++;
+// }
 
 // do - while
+do {
+    echo $counter . '<br>';
+    $counter++;
+} while ($counter < 0);
 
 // for
+for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br>';
+}
 
 // foreach
+$fruits = ['Banana', 'Apple', 'Orange'];
+foreach ($fruits as $i => $fruit) {
+    echo $i . ' ' . $fruit . '<br>';
+}
 
 // Iterate Over associative array.
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Video Games'],
+];
+foreach ($person as $key => $value) {
+    if (is_array($value)) {
+        echo $key . ': ' . implode(', ', $value)  . '<br>';
+    } else {
+        echo $key . ': ' . $value . '<br>';
+    }
+}
