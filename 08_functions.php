10ed48cfb8caeae4f89fd6ab55548803d7b807a5
Gulam Anas <g.anas786@gmail.com>
2022-01-30 13:14:44 +0530

    functions and arrow function

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,41 @@
 <?php
 
 // Function which prints "Hello I am Zura"
+function hello()
+{
+    echo "Hello I am Someone" . '<br>';
+}
+hello();
 
 // Function with argument
+function people($name)
+{
+    return "Hello I am $name";
+}
+echo people('Anas') . '<br>';
+echo people('Rohith') . '<br>';
 
 // Create sum of two functions
+// function sum($a, $b)
+// {
+//     return $a + $b;
+// }
+// echo sum(4, 5) . '<br>';
 
 // Create function to sum all numbers using ...$nums
+// function sum(...$nums)
+// {
+//     $sum = 0;
+//     foreach ($nums as  $n) {
+//         $sum += $n;
+//     }
+//     return $sum;
+// }
+// echo sum(1, 2, 3, 4, 5, 6);
 
 // Arrow functions
+function sum(...$nums)
+{
+    return array_reduce($nums, fn ($carry, $n) => $carry + $n);
+}
+echo sum(1, 2, 3, 4, 5, 6);
