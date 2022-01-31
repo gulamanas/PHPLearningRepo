0ccef95b21ea6ebc032753fc6829a86e788a8006
Gulam Anas <g.anas786@gmail.com>
2022-01-31 18:39:48 +0530

    magic constants

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,15 +1,23 @@
 <?php
 // Magic constants
+echo __DIR__ . '<br>'; // print the directory
+echo __FILE__ . '<br>'; // print the file name with path
+echo __LINE__ . '<br>'; // print the no. of line where it has been written
 
 // Create directory
+// mkdir('test');
 
 // Rename directory
+// rename('test', 'test2');
 
 // Delete directory
+// rmdir('test2');
 
 // Read files and folders inside directory
+echo file_get_contents('lorem.txt');
 
 // file_get_contents, file_put_contents
+
 
 // file_get_contents from URL
 
