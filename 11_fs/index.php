dc52d486cad4cab99e4530844f625b8361fc6b04
Gulam Anas <g.anas786@gmail.com>
2022-01-31 20:57:36 +0530

    file system

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -14,12 +14,20 @@ echo __LINE__ . '<br>'; // print the no. of line where it has been written
 // rmdir('test2');
 
 // Read files and folders inside directory
-echo file_get_contents('lorem.txt');
+$files = scandir('./');
+// $files = scandir('../');
+echo '<pre>';
+var_dump($files);
+echo '</pre>';
 
 // file_get_contents, file_put_contents
-
+echo file_get_contents('lorem.txt');
+file_put_contents('sample.txt', 'some content');
 
 // file_get_contents from URL
+// $usersJson = file_get_contents('https://jsonplaceholder.typicode.com/posts');
+// echo $usersJson;
+
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
