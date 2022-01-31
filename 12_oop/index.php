c18fad77dd5c9a54cf7783ec722ec50c86e6c89f
Gulam Anas <g.anas786@gmail.com>
2022-01-31 22:37:46 +0530

    class

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -1,6 +1,21 @@
 <?php
 
+
 // What is class and instance
+require_once 'Person.php';
+
+$p = new Person("Gulam", 'Anas');
+$p->setAge(23);
+echo '<pre>';
+var_dump($p);
+echo '</pre>';
+echo $p->getAge();
+
+$p2 = new Person('John', 'Smith');
+echo '<pre>';
+var_dump($p2);
+echo '</pre>';
+echo Person::$counter;
 
 // Create Person class in Person.php
 
