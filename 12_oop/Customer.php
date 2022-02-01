46812d0070efa8c8df33b0e41653859d022b8278
Gulam Anas <g.anas786@gmail.com>
2022-02-01 21:44:57 +0530

    construct and destruct

--- 12_oop/Customer.php
+++ 12_oop/Customer.php
@@ -7,12 +7,22 @@ class Customer
     public $email;
     public $balance;
 
+    public function __construct()
+    {
+        echo "The construcor ran....";
+    }
+
     public function getCustomer($id)
     {
         $this->id = $id;
         return 'John Doe';
     }
+
+    public function __destruct()
+    {
+        echo 'the destruct ran.....';
+    }
 }
 
 $customer = new Customer;
-echo $customer->getCustomer(111);
+echo $customer->getCustomer(1);
