ac56215c26626c9d020df11dcf341ea4ea9accb3
Gulam Anas <g.anas786@gmail.com>
2022-02-02 22:28:05 +0530

    curl

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -1,7 +1,17 @@
 <?php
 
 $url = 'https://jsonplaceholder.typicode.com/users';
+
 // Sample example to get data.
+$resource = curl_init($url);
+curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($resource);
+$code = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+echo '<pre>';
+var_dump($code);
+echo '</pre>';
+curl_close($resource);
+echo $result;
 
 // Get response status code
 
