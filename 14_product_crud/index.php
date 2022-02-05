8a9dae8b5aaef357c86f1b1e173a1619832b358c
Gulam Anas <g.anas786@gmail.com>
2022-02-05 23:34:04 +0530

    update products

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -56,7 +56,7 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
                     <td><?php echo $product['price'] ?></td>
                     <td><?php echo $product['create_date'] ?></td>
                     <td>
-                        <button class="btn btn-sm btn-outline-primary">Edit</button>
+                        <a href="update.php?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
                         <form style="display: inline-block;" method="post" action="delete.php">
                             <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
                             <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
---
+++ 14_product_crud/update.php
@@ -0,0 +1,138 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$id = $_GET['id'] ?? null;
+
+if (!$id) {
+    header('Location: index.php');
+    exit;
+}
+
+$statement = $pdo->prepare('SELECT * FROM products WHERE id = :id');
+$statement->bindValue(':id', $id);
+$statement->execute();
+$product = $statement->fetch(PDO::FETCH_ASSOC);
+
+$errors = [];
+
+$title = $product['title'];
+$price = $product['price'];
+$description = $product['description'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+
+    if (empty($errors)) {
+        $image = $_FILES['image'] ?? null;
+        $imagePath = $product['image'];
+
+        if ($image && $image['tmp_name']) {
+
+            if ($product['image']) {
+                unlink($product['image']);
+            }
+
+            $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+            mkdir(dirname($imagePath));
+
+            move_uploaded_file($image['tmp_name'], $imagePath);
+        }
+
+        $statement = $pdo->prepare("UPDATE products SET title = :title, image = :image, description = :description, price = :price WHERE id = :id");
+
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', $imagePath);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+        header('Location: index.php');
+    }
+}
+
+function randomString($n)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $str = '';
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $str .= $characters[$index];
+    }
+
+    return $str;
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Product CRUD</title>
+</head>
+
+<body>
+    <a href="./index.php" class="btn btn-secondary">Get back to Products</a>
+
+    <h1>Update Product <b><?php echo $product['title'] ?></b></h1>
+
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <?php if ($product['image']) : ?>
+            <img src="<?php echo $product['image'] ?> " alt="update image" class="update-image">
+        <?php endif; ?>
+        <div class="form-group">
+            <label>Product Image</label> <br>
+            <input type="file" name="image">
+        </div>
+        <div class="form-group">
+            <label>Product Title</label>
+            <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        </div>
+        <div class="form-group">
+            <label>Product Description</label>
+            <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+        </div>
+        <div class="form-group">
+            <label>Product Price</label>
+            <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+        </div>
+        <button type="submit" class="btn mt-2 btn-primary">Submit</button>
+        <a href="./index.php" class="btn mt-2 btn-outline-secondary">Home</a>
+    </form>
+
+
+</body>
+
+</html>
